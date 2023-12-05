bf95db454f18a644c312183a6d67aa71ccb67380
Carl Durocher <hexenjag@gmail.com>
2023-12-04 21:44:08 -0500

    Enlever les prints pour le debug

--- packages/regression_model/regression_model/config/config.py
+++ packages/regression_model/regression_model/config/config.py
@@ -2,10 +2,6 @@ import pathlib
 
 import regression_model
 
-print("regression_model.__file__", regression_model.__file__)
-print(pathlib.Path(regression_model.__file__))
-print(pathlib.Path(regression_model.__file__).resolve())
-print(pathlib.Path(regression_model.__file__).resolve().parent)
 PACKAGE_ROOT = pathlib.Path(regression_model.__file__).resolve().parent
 TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
 DATASET_DIR = PACKAGE_ROOT / "datasets"
